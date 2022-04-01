e8d729f2243e5714f04d2f9c122ea16f4eefc85f
aca99 <aca99@LAPTOP-5O165F67>
2022-04-01 17:14:22 +0530

    First commit

---
+++ Day_2_intro_java/src/git_day2/Day_2_examples.java
@@ -0,0 +1,13 @@
+package git_day2;
+import java.util.Scanner;
+public class Day_2_examples {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		System.out.println("NAME = "+ name);
+		
+		sc.close();
+	}
+
+}
---
+++ Day_2_intro_java/src/git_day2/data_type_day2.java
@@ -0,0 +1,25 @@
+package git_day2;
+import java.util.Scanner;
+
+
+public class data_type_day2 {
+
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String name = sc.nextLine();
+		System.out.println(name);
+		
+		int a = sc.nextInt();
+		System.out.println(a);
+		
+		
+		
+		double x = sc.nextDouble();
+		System.out.println(x);
+ 
+		sc.close();
+	}
+
+}
