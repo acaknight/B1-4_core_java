3c9efde7c0b9d5163ad87254bfc6bc580b080dae
aca99 <aca99@LAPTOP-5O165F67>
2022-04-01 19:30:47 +0530

    First Commit

--- Day_5_inheritence/src/day5/HierchcialInherit.java
+++ Day_5_inheritence/src/day5/HierchcialInherit.java
@@ -15,14 +15,27 @@ class Samsung extends CellPhone
 {
 	void Facelock()
 	{
-		System.out.println("");
+		System.out.println("Modern Facelock");
+	}
+}
+class oneplus extends CellPhone
+{
+	void Game()
+	{
+		System.out.println("PUB-G");
 	}
 }
 public class HierchcialInherit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Samsung t = new Samsung();
+		oneplus p = new oneplus();
+		p.message();
+		t.message();
+		p.call();
+		p.Game();
+		
 	}
 
 }
