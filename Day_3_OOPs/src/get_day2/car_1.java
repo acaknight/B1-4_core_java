cc46095e2023206e94ebcdefd186453ac803de2d
aca99 <aca99@LAPTOP-5O165F67>
2022-04-01 17:13:41 +0530

    First commit

---
+++ Day_3_OOPs/src/get_day2/car_1.java
@@ -0,0 +1,20 @@
+package get_day2;
+import java.util.*;
+
+public class car_1 {
+	String str;
+	void Car1 (String s)
+	{	
+		str = s;
+	}
+	 void print()
+	 {
+		 System.out.println(" This is ");
+	 }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car1 c = new Car1("Audi");
+		Car1 c1 = new Car1("bmw");
+	}
+
+}
