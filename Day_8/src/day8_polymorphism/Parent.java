23dc287c6b3120509e63eafdf2e2a02f8e48764e
aca99 <aca99@LAPTOP-5O165F67>
2022-04-07 16:36:54 +0530

    First Commit

---
+++ Day_8/src/day8_polymorphism/Parent.java
@@ -0,0 +1,27 @@
+package day8_polymorphism;
+
+class Base 
+{
+	void method1()
+	{
+		System.out.println("Parent1");
+	}
+}
+class Derived extends Base
+{
+	void method1()
+	{
+		System.out.println("Child1");
+	}
+}
+class Parent
+{
+	
+	public static void main(String[] args) {
+		Base b=new Base();
+		b.method1();
+		Derived d=new Derived();
+		d.method1();
+
+	}
+}
