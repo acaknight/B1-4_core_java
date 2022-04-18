c9fcb4f28217f1d758f340ffb8de2a13d7126443
aca99 <aca99@LAPTOP-5O165F67>
2022-04-18 17:16:49 +0530

    First commit

---
+++ Day_15/src/day15_collection/example4.java
@@ -0,0 +1,16 @@
+package day15_collection;
+
+import java.util.Vector;
+
+public class example4 {
+	
+	public static void main(String[] args) {
+		Vector v=new Vector();
+		v.add("Souajan");
+		v.add(11);
+		System.out.println(v);
+
+
+	}
+
+}
---
+++ Day_15/src/day15_collection/example5.java
@@ -0,0 +1,18 @@
+package day15_collection;
+
+import java.util.Stack;
+
+public class example5 {
+	
+	public static void main(String[] args) {
+		Stack s=new Stack();
+		s.add(10);
+		s.add(20);
+		s.pop();
+		System.out.println(s);
+
+
+	}
+
+
+}
